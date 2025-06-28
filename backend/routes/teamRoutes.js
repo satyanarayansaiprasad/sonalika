ea902b56357f046f59c937b84c620e97e6de056e
Debabrat Swain <swaindebabratt@gmail.com>
2025-06-29 02:23:06 +0530

    update

--- backend/routes/teamRoutes.js
+++ backend/routes/teamRoutes.js
@@ -5,7 +5,7 @@ const TeamController = require('../controllers/TeamController');
 router.post('/productionteam-login',TeamController.loginProduction)
 router.post('/salesteam-login',TeamController.loginSalesteam)
 router.post('/create-client',TeamController.createUser)
- router.get('/get-clients/:uniqueId',TeamController.getAllClients)
+ router.get('/get-clients',TeamController.getAllClients)
  router.post('/client-orders',TeamController.addClientOrder)
 router.get('/order-history/:uniqueId', TeamController.getOrderHistory)
 module.exports = router;

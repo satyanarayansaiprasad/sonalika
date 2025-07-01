003b90475fe6833a324211f6be4e21c3c31874c9
Debabrat Swain <swaindebabratt@gmail.com>
2025-07-01 20:06:11 +0530

    update

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -66,7 +66,7 @@ const handleSubmit = async (e) => {
       },
       { 
         withCredentials: true,
-        timeout: 10000
+        timeout: 20000
       }
     );
     console.timeEnd('axiosRequest');

b12773429a856746f978efe7a51f33441a395318
Debabratt <swaindebabratt@gmail.com>
2025-07-28 22:26:39 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Connect MongoDB
 connectDB();
-
+app.use(express.json());
 // Increase payload size limit for file uploads
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));

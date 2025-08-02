cb6ef26027f26bacb5e6c08ef9dad48ffbda4d7b
Debabratt <swaindebabratt@gmail.com>
2025-08-03 01:46:42 +0530

    update

--- backend/models/ProductMaster.js
+++ backend/models/ProductMaster.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const ProductMasterSchema = new mongoose.Schema({
   serialNumber: { type: String, required: true, unique: true },
   category: { type: String, required: true },
-  sizeType: { type: String },
-  sizeValue: { type: String },
+  types: { type: String },
+  values: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 

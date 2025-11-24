eb6d22681e620f1f246a9fa547b1d0a621152012
user.name: "satyanarayan saiprasad <user.email: "satyanarayansaiprasadofficial@gmail.com>
2025-11-24 15:18:55 +0530

    Remove AdminDashboard, update ProtectedRoute roles for Production and Sales logins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import TeamLogin from './Components/TeamLogin';
 import SalesTeamDashboard from './Components/SalesTeamDashboard';
-import AdminDashboard from './Components/AdminDashboard';
 import ProductionTeam from './Components/ProductionTeam';
 import ProtectedRoute from './Components/ProtectedRoute';
 
@@ -19,22 +18,16 @@ const App = () => {
 
        
         <Route  path='/spteamlogin'  element={<TeamLogin />}/>
-       <Route path='/admindashboard'  element={
-          <ProtectedRoute allowedRoles={["admin"]}>
-            <AdminDashboard />
-          </ProtectedRoute>
-        }/> 
-         {/* <Route path='/admindashboard'  element={
-          
-            <AdminDashboard />
-         
-        }/> */}
       <Route  path='/salesteamdashboard' element={
-        <ProtectedRoute allowedRoles={["team"]}>
-        <SalesTeamDashboard/>
+        <ProtectedRoute allowedRoles={["salesteam"]}>
+          <SalesTeamDashboard/>
         </ProtectedRoute>
-        }/>
-      <Route  path='/productiondashboard' element={<ProductionTeam/>}/>
+      }/>
+      <Route  path='/productiondashboard' element={
+        <ProtectedRoute allowedRoles={["productionteam"]}>
+          <ProductionTeam/>
+        </ProtectedRoute>
+      }/>
       {/* Add more routes here when needed */}
     </Routes>
   );
